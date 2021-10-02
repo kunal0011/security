2ebfcceb274774ce64321765aa6a6f79a49104d5
kunal <kunalkumar1034@gmail.com>
2021-10-02 12:42:48 +0530

    security basic

---
+++ src/main/java/com/example/config/MyWebSecurityConfigAdapter.java
@@ -0,0 +1,46 @@
+package com.example.config;
+
+import com.example.service.CustomAuthenticationProviderService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
+
+@Configuration
+@Log4j2
+@EnableWebSecurity
+public class MyWebSecurityConfigAdapter extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private CustomAuthenticationProviderService authenticationProvider;
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SCryptPasswordEncoder sCryptPasswordEncoder() {
+        return new SCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(authenticationProvider);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().disable();
+        http.formLogin()
+                .defaultSuccessUrl("/main", true);
+        http.authorizeRequests().anyRequest().authenticated();
+    }
+}
---
+++ src/main/java/com/example/service/ProductService.java
@@ -0,0 +1,18 @@
+package com.example.service;
+
+import com.example.entity.Product;
+import com.example.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+}
